053c26f1d1e6d182d055541858eff48d07cc64fa
Madu Manathunga <mmmskumara@gmail.com>
2024-09-30 17:44:14 -0400

    Revert "Updated"
    This reverts commit b420dea65969f6f2b23cc5ddefc17c83c944fa03.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,6 @@ extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc", "r
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-html_baseurl = 'https://attmos.github.io/AFFDOWS'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
